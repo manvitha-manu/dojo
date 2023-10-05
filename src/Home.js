e290901c0c4da8a77d7c5ee5e8dac84fc257824d
Manvitha_Golla <manvithagc@gmail.com>
2023-10-05 12:43:16 -0700

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const[blogs,setBlogs]=useState([{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
-    
 
-    
-    return ( 
-        <div className="home">
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id} >
-              <h2>{ blog.title }</h2>
-              <p>Written by { blog.author }</p>
-            </div>
-    ))}
+  const [name, setName] = useState('mario');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [name])
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
     </div>
-    )
+  );
 }
  
 export default Home;
